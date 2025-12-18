6eca22539a100c8c2a2e7f2a79de362e567e05b3
haruhisayaguchi <hal.yaguchi@gmail.com>
2025-12-17 16:40:12 -0800

    [add] section_03

---
+++ section_03/nestjs-intro/src/users/users.controller.ts
@@ -0,0 +1,50 @@
+import {
+	Controller,
+	Get,
+	Post,
+	Patch,
+	Put,
+	Delete,
+	Param,
+	Query,
+	Body,
+	Headers,
+	Ip,
+	ParseIntPipe,
+	DefaultValuePipe,
+	ValidationPipe,
+} from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { GetUsersParamDto } from './dtos/get-users-param.dto';
+import { PatchUserDto } from './dtos/patch-user.dto';
+
+@Controller('users')
+export class UsersController {
+
+	@Get('{/:id}')
+	public getUsers(
+		@Param() getUsersParamDto: GetUsersParamDto,
+		@Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+		@Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+	) {
+		console.log(typeof getUsersParamDto, getUsersParamDto);
+		console.log(typeof limit, limit);
+		console.log(typeof page, page);
+		return "You sent a get request to users endpoint";
+	}
+
+	@Post()
+	public createUsers(
+		@Body() createUserDto: CreateUserDto,
+	) {
+		console.log(createUserDto instanceof CreateUserDto);
+		return "You sent a post request to users endpoint";
+	}
+
+	@Patch()
+	public patchUser(
+		@Body() patchUserDto: PatchUserDto
+	) {
+		return patchUserDto;
+	}
+}
